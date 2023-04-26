1a25daf1d3251b49dd190656a7fb36882ba5bfeb
Nine-777 <hkearth777@gmail.com>
2023-04-26 12:37:15 +0900

    余計なコード削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header />
     </BrowserRouter>
   );
 }
