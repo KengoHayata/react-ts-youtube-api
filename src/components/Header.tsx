cd0071109849d66c3aa1854c48661331423e4db9
Nine-777 <hkearth777@gmail.com>
2023-04-26 12:37:06 +0900

    TOPボタン/ログインボタンのスタイリング

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <Button variant="contained" color="primary" component={Link} to="/">
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ top: 20, left: 60 }}
+        component={Link}
+        to="/"
+      >
         TOP
       </Button>
       <Button
         variant="contained"
         color="primary"
-        component={Link}
-        to="https://accounts.google.com/o/oauth2/v2/auth"
+        sx={{ position: "absolute", top: 20, right: 60 }}
       >
         ログイン
       </Button>
