f779d2c525b65da11d3477860bac63e6a464ffc1
KengoHayata <kengo.hayata@irsc.jp>
2023-05-24 12:46:58 +0900

    動画を表示するコンポーネントを作成

--- src/components/ShowVideoIndex.tsx
+++ src/components/ShowVideoIndex.tsx
@@ -1,38 +1,40 @@
 import axios from "axios";
-import MyAppBar from "./ui/MyAppBar";
-import MyButton from "./ui/MyButton";
 import { useEffect, useState } from "react";
+import { Card, CardContent, CardMedia, Stack } from "@mui/material";
 
 export const ShowVideoIndex = () => {
   const [data, setData] = useState<any>();
 
-  // const getPateleVideos = () => {
-  //   axios.get().then((res) => {
-  //     setData(res.data);
-  //   });
-  // };
+  const getVideos = () => {
+    axios.get("").then((res) => {
+      setData(res.data);
+    });
+  };
 
-  // useEffect(() => {
-  //   getPateleVideos();
-  // }, []);
+  useEffect(() => {
+    getVideos();
+  }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexWrap: "wrap",
-        marginTop: 34,
-      }}
-    >
-      {/* getExampleComを実行するボタン */}
+    <Stack direction="row" spacing={2}>
       {data?.items.map((item: any) => (
-        <>
-          <div>{item.snippet.title}</div>
-          <img src={item.snippet.thumbnails.default.url} alt="thumbnails" />
-        </>
+        <Card
+          sx={{
+            width: "33.333333%",
+          }}
+        >
+          <CardMedia
+            component="img"
+            height="194"
+            image={item.snippet.thumbnails.default.url}
+            alt="thumbnails"
+          />
+          <CardContent>
+            <div>{item.snippet.title}</div>
+            <div>{item.snippet.channelTitle}</div>
+          </CardContent>
+        </Card>
       ))}
-    </div>
+    </Stack>
   );
 };
